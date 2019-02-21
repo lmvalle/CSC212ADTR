6f7bccd643194aba738551436b465e3cffb6fbc1
volcano <volcano@131.229.142.212>
2019-02-21 14:48:31 -0500

    complete, testSet, on testMap

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -7,9 +7,34 @@ import edu.smith.cs.csc212.adtr.real.JavaMap;
 
 public class JavaMapTest {
 
+	void assertIntEq(int x, int y) {
+		assertEquals(x,y);
+	}
 	@Test
 	public void testEmpty() {
 		MapADT<String, Integer> empty = new JavaMap<>();
 		assertEquals(empty.size(), 0);
+	}
+	@Test
+	public void testPut() {
+		MapADT<String, Integer> vari = new JavaMap<>();
+		vari.put("a", 1);
+		vari.put("A", 1);
+		assertEquals(vari.size(), 2);
+	}
+	@Test
+	public void testGet() {
+		MapADT<String, Integer> vari = new JavaMap<>();
+		vari.put("a", 1);
+		vari.put("A", 1);
+		assertEquals(vari.get("a"),vari.get("A"));
+	}
+	@Test
+	public void testRemove() {
+		MapADT<String,Integer> vari = new JavaMap<>();
+		vari.put("a",1);
+		vari.put("A", 1);
+		vari.remove("A");
+		assertEquals(vari.size(),4s);
 	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -2,9 +2,12 @@ package edu.smith.cs.csc212.adtr;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Set;
+
 import org.junit.Test;
 
 import edu.smith.cs.csc212.adtr.real.JavaSet;
+
 
 public class JavaSetTest {
 	
@@ -12,5 +15,61 @@ public class JavaSetTest {
 	public void testEmpty() {
 		SetADT<String> empty = new JavaSet<>();
 		assertEquals(empty.size(), 0);
+	}
+	@Test
+	public void testInsert() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("a");
+		abc.insert("b");
+		abc.insert("c");
+		assertEquals(abc.size(),3);
+	}
+	@Test
+	public void testInsertRepeated() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("A");
+		abc.insert("A");
+		abc.insert("A");
+		assertEquals(abc.size(),1);
+	}
+	@Test
+	public void testSize() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("a");
+		abc.insert("b");
+		abc.insert("c");
+		abc.insert("d");
+		assertEquals(abc.size(),4);
+	}
+	@Test
+	public void testContains() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("e");
+		abc.insert("f");
+		abc.insert("g");
+		assertEquals(abc.contains("e"),true);
+	}
+	@Test
+	public void testRemove() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("a");
+		abc.insert("A");
+		abc.remove("A");
+		assertEquals(abc.size(), 1);
+	}
+	@Test
+	public void testToList() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("a");
+		ListADT<String> dfg = abc.toList();
+		assertEquals(dfg.size(),1);
+	}
+	@Test
+	public void testToJava() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("a");
+		abc.insert("b");
+		Set<String> dfg = abc.toJava();
+		assertEquals(dfg.size(), 2);
 	}
 }
